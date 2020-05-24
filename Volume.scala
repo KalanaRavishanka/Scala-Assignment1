548cc92a4cd10897563cf045b2d09eb9e2a587be
KalanaRavishanka <ultimatekalana@gmail.com>
2020-05-24 16:31:31 +0000

    did assignment

---
+++ Volume.scala
@@ -0,0 +1,6 @@
+
+object Volume extends App{
+ def calculate(x:Double)=4/3*3.14*x*x*x;
+ println(calculate(5));
+
+}
---
+++ price.scala
@@ -0,0 +1,6 @@
+
+object price extends App{
+ def cal(x:Double)=(x-x*40/100-3)*50+(x-x*40/100-3.75)*10
+ println(cal(24.95));
+
+}
